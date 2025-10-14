bd98f5d87e29732550edf774aa71de00783148ec
motta97 <155576275+motta97@users.noreply.github.com>
2025-10-14 21:07:27 +0300

    Create DisplayLLRecursively.cpp

---
+++ Data Structures/Linked Lists/DisplayLLRecursively.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+class linkeList{
+public:
+    int data;
+    linkeList *next;
+}*first=NULL;
+void create(int arr[],int n){
+    //we want to create a linked list having the data as the array
+    first = new linkeList;
+    first->data=arr[0];
+    linkeList *q=first;
+
+    for(int i =1; i<n;i++){
+
+        q->next=new linkeList;
+        q=q->next;
+        q->data=arr[i];
+        q->next=NULL;
+
+
+    }
+    
+
+
+}
+
+void displayRecursive(linkeList * p){
+    if(p==NULL){
+        return;
+    }
+    cout<<p->data<<endl;
+    displayRecursive(p->next);
+}
+
+int main(){
+int arr[]={5,4,3,2,1};
+create(arr,5);
+displayRecursive(first);
+
+}
