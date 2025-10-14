bc0c66bc6b605789add725d1d4e2c800c2d93cf8
motta97 <155576275+motta97@users.noreply.github.com>
2025-10-14 20:49:49 +0300

    Create createALinkedList.cpp

---
+++ Data Structures/Linked Lists/createALinkedList.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class linkeList{
+public:
+    int data;
+    linkeList *next;
+}*first=NULL;
+void create(int arr[],int n){
+    //we want to create a linked list having the data as the array
+    first = new linkeList;
+    first->data=arr[0];
+    linkeList *q=first;
+
+    for(int i =1; i<n;i++){
+
+        q->next=new linkeList;
+        q=q->next;
+        q->data=arr[i];
+        q->next=NULL;
+
+
+    }
+
+}
+void displayLL(linkeList *l){
+linkeList *tmp = l;
+while(tmp){
+    cout<<tmp->data<<endl;
+    tmp=tmp->next;
+}
+
+
+}
+
+int main(){
+int arr[]={5,4,3,2,1};
+create(arr,5);
+displayLL(first);
+
+}
