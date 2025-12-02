7c934127fad7dacf922f04d1b978f413e60c3b9e
motta97 <155576275+motta97@users.noreply.github.com>
2025-12-02 18:18:39 +0200

    Fix DFS to check if vertex is unvisited before recursion

--- Data Structures/Graph/DFS_Adjacency_Mtrx.cpp
+++ Data Structures/Graph/DFS_Adjacency_Mtrx.cpp
@@ -2,10 +2,10 @@ void DFS(int i){
     if(visited[i]==0){
         visited[i]=1;
         for(int v =1;v<=n;v++){
-            if(m[i][v]==1){
-                visited[v]=1;
+            if(m[i][v]==1&&visited[v]==0){
                 DFS(v);
             }
+        
         }
     }
 }
