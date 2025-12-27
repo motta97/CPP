7a3cf99d4c5b2f00371742d105ab8b6cc9a58c3b
motta97 <155576275+motta97@users.noreply.github.com>
2025-12-27 15:29:08 +0200

    Add delete_from_min_heap function
    Implement function to delete an element from a min heap.

---
+++ Data Structures/Heap/delete_from_min_heap.cpp
@@ -0,0 +1,26 @@
+void delete_from_min_heap(int A[],int n){
+    //A is the array of the heap
+    //n is the last index
+    int x,i,j,tmp;
+    x=A[n];//the last element
+    A[1]=x;//the first element is the element to be removed
+    i=1;
+    j=2*i;
+
+    while(j<n-1){
+
+        if(A[j+1]<A[j]){
+            j+=1;
+        }
+        if(A[i]>A[j]){
+            //swap
+            tmp =A[i];
+            A[i]=A[j];
+            A[j]=tmp;
+
+            i=j;
+            j=2*i;
+        }
+        else break;
+    }
+}
