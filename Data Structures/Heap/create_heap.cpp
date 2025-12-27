6ae470da7d9b00a36094954fc9283337fae8b331
motta97 <155576275+motta97@users.noreply.github.com>
2025-12-27 14:26:04 +0200

    Add create_heap and insert functions
    Implement heap creation and insertion functions.

---
+++ Data Structures/Heap/create_heap.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+void insert(vector<int> A,int n){
+    int tmp,i=n;
+    tmp = A[n];
+    while(i>1 &&tmp>A[i/2]  ){
+        A[i]=A[i/2];
+        i/=2;
+    }
+    A[i]=tmp;
+}
+
+void create_heap(vector<int> v){
+    //assuming that the index 0 is not of interest as for typical heap use
+  //the input is a normal vector that is not arranged as heap
+  //we loop over all elements to form a heap
+    int i;
+    for(i=2;i<v.size();i++){
+        insert(v,i);
+    }
+}
